7d6b315e5102e9aa44d625990b887cce18c0dcaa
Evgeny Barabanov <evgeny.barabanov@intel.com>
2016-07-04 17:07:47 +0300

    - Reorganize sources - Add pom.xml

---
+++ src/main/java/com/evgenbar/binarydecoder/Decodable.java
@@ -0,0 +1,10 @@
+package com.evgenbar.binarydecoder;
+
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Decodable {
+	
+}
